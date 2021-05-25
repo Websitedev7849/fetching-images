eee248c608f6e4c6d8033a4b1404e7ff041ce3c9
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-05-25 16:40:20 +0530

    sending response is successfull but receiving failed

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const fetch = require('node-fetch');
 
-const url = 'http://localhost:3300/get-stock';
+const url = 'http://localhost:3300/';
 
 (async function(){
     try {
         let res = await fetch(url);
-        let data = await res.buffer();
-        console.log(data.toString());
+        let data = res.buffer();
+        console.log(data);
     } catch (err) {
         console.log("there was an error\n");
+        console.log(err);
     }
 })();
---
+++ sendImage.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 3300;
+
+const server = http.createServer((req,res)=>{
+    console.log(`request was made ${req.url}`);
+    switch (req.url) {
+        case '/':
+            res.write("hi wasupp?")
+            console.log(`req was made to ${req.url}`);
+            break;
+    }
+});
+
+server.listen(PORT, 'localhost',()=>{
+    console.log(`listnening to sever on ${PORT}`);
+});
