5974c9767fb8b9f642a8e89bb40d11a0af749f70
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-05-26 15:38:42 +0530

    successfull in diplaying image in browser, but failed in writing new image(copy_of_image.jpg)

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 const url = 'http://localhost:3300/';
 
 (async function(){
     try {
-        fetch(url)
-        .then(res => res.text())
-        .then(body => console.log(body))
+        let res = await fetch(url);
+        let data = await res.text();
+        fs.writeFile('./files/copy_of_photo.jpg', data, (err)=>{
+            (err)? console.log("some error"): console.log("no error") ; 
+        })
+
     } catch (err) {
         console.log("there was an error\n");
         console.log(err);
--- sendImage.js
+++ sendImage.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const PORT = 3300;
 
@@ -6,7 +8,11 @@ const server = http.createServer((req,res)=>{
     console.log(`request was made ${req.url}`);
     switch (req.url) {
         case '/':
-            res.end("hi wasupp?")
+            res.writeHead(200, {'Content-Type' : 'image/jpg'})
+            fs.readFile("./files/photo.jpg", (err, data)=>{
+                res.end(data)
+            });
+            
             console.log(`req was made to ${req.url}`);
             break;
     }
